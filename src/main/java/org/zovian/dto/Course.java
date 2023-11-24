3d9e9e74f6ebc26f48be4d1a8f50e5bd1e4facc4
dzovi <2389589@edu.vaniercollege.qc.ca>
2023-11-24 16:16:55 -0500

    some methods added

--- src/main/java/org/zovian/dto/Course.java
+++ src/main/java/org/zovian/dto/Course.java
@@ -10,12 +10,25 @@ import lombok.ToString;
 @Getter
 @Setter
 public class Course {
+    private static final int MAX_STUDENT_NUM = 5;
     private double credit;
     private String id;
-    private Student[] students; //MAX NUM
+    private Student[] students;
     private Department department;
     private int studentNum;
     private Teacher teacher;
     private String courseName;
+
+    public Course(String courseName) {
+
+    }
+
+    public void addStudent(Student student){
+
+    }
+
+    public void addTeacher(Teacher teacher) {
+
+    }
 
 }
