136caf3d114376f3df77df0700ffc7f10d6e4e45
dzovi <2389589@edu.vaniercollege.qc.ca>
2023-11-25 02:08:30 -0500

    most of the methods are added

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 #  SchoolManagementSystem
+A school management system that contains departments, students, teachers, and courses within a school, 
+with each being interlinked together.
+
+The final project for Introduction to Programming course (420-201-RE)
+Done by using Object-Oriented-Programming (OOP) and the knowledge points covered throughout the course.
+
+![img.png](img.png)
--- src/main/java/org/zovian/dto/SchoolManagementSystem.java
+++ src/main/java/org/zovian/dto/SchoolManagementSystem.java
@@ -5,6 +5,9 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+/**
+ * manages departments, students, teachers and courses within a school
+ */
 @ToString
 @EqualsAndHashCode
 @Getter
@@ -20,6 +23,9 @@ public class SchoolManagementSystem {
     private Teacher[] teachers;
     private Course[] courses;
 
+    /**
+     * constructs a new SchoolManagementSystem with empty lists for departments, teachers,students and courses
+     */
     public SchoolManagementSystem() {
 
     }
@@ -42,47 +48,88 @@ public class SchoolManagementSystem {
 
     }
 
-    //methods to display ...
+    /**
+     * displays information about all departments in the school
+     */
     public void printDepartments() {
 
     }
 
+    /**
+     * displays information about all students in the school, including their registered courses.
+     */
     public void printStudents() {
 
     }
 
+    /**
+     * displays information about all teachers in the school, including the courses they teach
+     */
     public void printTeachers() {
 
     }
 
+    /**
+     * displays information about all courses in the school, including the teacher, students,
+     * and department associated with each course
+     */
     public void printCourses() {
 
     }
 
-    //search for methods
-
+    /**
+     * Searches for a department in the school based on the provided departmentId.
+     * @param departmentId
+     * @return the Department with the specified ID, or null if not found.
+     */
     public Department findDepartment(String departmentId) {
         return null;
     }
 
+    /**
+     * Searches for a teacher in the school based on the provided teacherId.
+     * @param teacherId
+     * @return he Teacher with the specified ID, or null if not found.
+     */
     public Teacher findTeacher(String teacherId) {
         return null;
     }
 
+    /**
+     * Searches for a course in the school based on the provided courseId.
+     * @param courseId
+     * @return The Course with the specified ID, or null if not found.
+     */
     public Course findCourse(String courseId) {
         return null;
     }
 
+    /**
+     * Searches for a student in the school based on the provided studentId.
+     * @param studentId
+     * @return The Student with the specified ID, or null if not found.
+     */
     public Student findStudent(String studentId) {
         return null;
     }
 
-    //assign teacher to a course
+    /**
+     * Assigns a teacher to a specific course based on the provided teacherId and courseId.
+     * Prints a message if either the teacher or the course is not found.
+     * @param teacherId
+     * @param courseId The ID of the course to which the teacher should be assigned.
+     */
     public void modifyCourseTeacher(String teacherId, String courseId) {
 
     }
 
-    //register a student for a course
+    /**
+     *  Registers a course for a student based on the provided studentId and courseId.
+     *  Checks various conditions, prints messages accordingly, and updates the student
+     *  and course information if the registration is successful.
+     * @param studentId
+     * @param courseId
+     */
     public void registerCourse(String studentId, String courseId) {
 
     }
