caa90ea9b7e694ab6240330b6d500a829fa4f2af
dzovi <2389589@edu.vaniercollege.qc.ca>
2023-12-05 19:29:37 -0500

    all documentations

--- src/main/java/org/zovian/Main.java
+++ src/main/java/org/zovian/Main.java
@@ -2,6 +2,9 @@ package org.zovian;
 
 import org.zovian.dto.*;
 
+/**
+ * @author Zovian Shagdjian
+ */
 public class Main {
     public static void main(String[] args) {
         SchoolManagementSystem smSystem = new SchoolManagementSystem();
